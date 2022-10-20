839552c32c04b029e9a55ffd64715b0c8698ec9d
hungnt09kaios <81213505+hungnt09kaios@users.noreply.github.com>
2022-10-20 22:48:49 +0700

    Add files via upload
    createnewgift

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hương khum Thối';
+const giftUrl = '';
+const eventName = 'Mừng ngày 20/10';
+const titleCard = ' Dear Peace ';
+const contentCard = 'Hế lô. Hổng giỏi thơ, cũng hổng giỏi văn, nên hổng biết chúc như nào hết á ^^ Thôi thì có gì viết đấy, lỡ có giống văn mẫu ở đâu đấy thì thông cảm nhá. Nhân ngày 20/10 thì cũng bạn tui luôn xinh đẹp nè, bớt xì trét nè, luôn dzui dzẻ, và đạt được mọi thứ như bạn mong muốn nhaaa. Tóm cái váy lại là chúc bạn có tất cả hehe 💘 ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'huongne.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
