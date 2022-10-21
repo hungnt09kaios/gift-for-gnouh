7920a52d325eea2bbe7aecdcd9b0017a96a7ac19
hungnt09kaios <81213505+hungnt09kaios@users.noreply.github.com>
2022-10-21 13:10:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,6 @@ const contentCard = 'Hế lô. Hổng giỏi thơ, cũng hổng giỏi văn, nê
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 const giftImage = 'huongne.jpg';
+const minigiftImage = 'um.jpg'
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
