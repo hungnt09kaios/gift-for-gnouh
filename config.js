3552ff9b2ba446c7ac6833779f496a0a2c7591b3
hungnt09kaios <81213505+hungnt09kaios@users.noreply.github.com>
2022-10-22 00:56:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Hương khum Thối';
 const giftUrl = '';
 const eventName = 'Mừng ngày 20/10';
 const titleCard = ' Dear Peace ';
-const contentCard = 'Hế lô. Hổng giỏi thơ, cũng hổng giỏi văn, nên hổng biết chúc như nào hết á ^^ Thui thì có gì viết nấy, lỡ có giống văn mẫu ở đâu đấy thì thông cảm heng 🤟 Nhân ngày 20/10 chúc bạn tui luôn xinh đẹp nè, bớt xì trét nè, luôn dzui dzẻ, và đạt được mọi thứ như bạn mong muốn nhaaa. Tóm cái váy lại là chúc bạn có tất cả hehe 💐💐💐🎁🎁🎁🎉🎉🎉';
+const contentCard = 'Hế nhô 👋 Hổng giỏi thơ, cũng hổng giỏi văn, nên hổng biết chúc như nào hết á ^^ Thui thì có gì viết nấy, lỡ có giống văn mẫu ở đâu đấy thì thông cảm heng 🤟 Nhân ngày 20/10 chúc bạn tui luôn xinh đẹp nè, bớt xì trét nè, luôn dzui dzẻ, và đạt được mọi thứ như bạn mong muốn nhaaa. Tóm cái váy lại là chúc bạn có tất cả hehe 💐💐💐🎁🎁🎁🎉🎉🎉';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
